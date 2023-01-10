8000ac76a34a29c1793f1ecd6b87e7eee9f45860
Cyrul1 <mateuszc076@wp.pl>
2023-01-10 12:05:28 +0100

    Basic learning

--- src/Controller/hellocontroler.php
+++ src/Controller/hellocontroler.php
@@ -10,15 +10,16 @@ class HelloController
         "Hello", "Hi!", "bye"
     ];
 
-    #[Route('/', name: 'app_index')]
-    public function index()
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
     {
-        return new Response(implode(',', $this->messages));
+        return new Response(
+            implode(',', array_slice($this->messages, 0 , $limit)));
     }
     
-    #[Route('/messages/{id}', name: 'app_show_one')]
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
 
-    public function showOne($id): Response
+    public function showOne(int $id): Response
     {
         return new Response($this->messages[$id]);
     }
