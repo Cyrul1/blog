9dbb503b729f228f766697aebf099e90ff2987c7
Cyrul1 <mateuszc076@wp.pl>
2023-01-13 15:03:44 +0100

    editing post

--- src/Controller/SocialController.php
+++ src/Controller/SocialController.php
@@ -9,29 +9,15 @@ use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class SocialController extends AbstractController
 {
     #[Route('/social', name: 'app_social')]
     public function index(MicroPostRepository $posts): Response
     {
-    //dd($posts->findAll());  
-     /*   $microPost = new MicroPost();
-        $microPost-> setTitle('it comes from controler');
-        $microPost-> setText('to wychodzi z controlera');
-      $microPost-> setCreated(new DateTime());
-      /*
-    #    $microPost = $posts->find(4);
-        #$microPost->setTitle('Welcom in general');
 
-        $posts->save($microPost, True);
-       /* $microPost = $posts->find(4);
-        $microPost->setTitle('Welcom in general');
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($microPost);
-        $em->flush();
-        */
         return $this->render('social/indexx.html.twig', [
             'posts' => $posts->findAll(),
         ]);
@@ -43,5 +29,63 @@ class SocialController extends AbstractController
             'post' => $post,
         ]);
     }
+    #[Route('/social/add', name: 'app_social_add', priority:2 )]
+    public function add(Request $request, MicroPostRepository $posts): Response
+    {
+        $micropost = new MicroPost();
+        $form = $this->createFormBuilder($micropost)
+        ->add('title')
+        ->add('text')
+        //->add('submit', SubmitType::class, ['label' => 'save'])
+        ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post, true);
+
+            $this->addFlash('success', 'Twój post został dodany!');
+            
+            return $this-> redirectToRoute('app_social');
+        }
+
+        return $this->renderForm(
+            'social/add.html.twig',
+            [
+                'form' => $form
+            ]
+            );  
+    }
+
+    #[Route('/social/{post}/edit', name: 'app_social_edit')]
+    public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
+    {
+        
+        $form = $this->createFormBuilder($post)
+        ->add('title')
+        ->add('text')
+        //->add('submit', SubmitType::class, ['label' => 'save'])
+        ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $post = $form->getData();
+            
+            $posts->save($post, true);
+
+            $this->addFlash('success', 'Twój post został zaktualizownay!');
+            
+            return $this-> redirectToRoute('app_social');
+        }
+
+        return $this->renderForm(
+            'social/edit.html.twig',
+            [
+                'form' => $form
+            ]
+            );  
+    }
+
 }
 ?>
